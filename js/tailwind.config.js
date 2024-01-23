3c104fbe9e11b7bd4127de318350495b253a3019
Shahid Ahamed <154579839+Shahid2890@users.noreply.github.com>
2024-01-23 16:37:31 +0900

    index.html

---
+++ js/tailwind.config.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        daisyui: {
+          themes: ["light", "dark", "cupcake"],
+        },
+        colors: {
+          primary: "#131318",
+          secondary: "#FF4240",
+          textColor: "rgba(19, 19, 24, 0.6)",
+          paraText: "rgba(255, 255, 255, 0.60)",
+          navLink: "#424247",
+          borderColor: "rgba(19, 19, 24, 0.2)",
+        },
+        fontFamily: {
+          manrope: "'Manrope', sans-serif",
+        },
+        maxWidth: {
+          container: "1170px",
+        },
+        backgroundImage: {
+          bannerImg: "url('../images/banner_img.png')",
+        },
+      },
+    },
+  };
